537d889f244ac1ceda8df32ba63c328bd8a9f354
James <hoffmannjamesjr@gmail.com>
2023-09-20 13:28:29 -0700

    Update en.lua

--- locale/en.lua
+++ locale/en.lua
@@ -1,10 +1,41 @@
 Locale = {}
 Locale.Errors = {
-    not_bondsman = 'You are not a bondsman.'
+    not_bondsman = "You are not a bondsman.",
+    not_police = "You are not a police officer.",
+    not_in_city = "This person is not currently in the city.",
+    too_far_away = "You are too far away from this player!",
+    not_enough_money = "You do not have enough money in your bank!",
+    dont_owe = "You don't owe us money.",
+    too_much = "You don't owe us this much!",
+    no_longer_needs = "This person no longer needs bail.",
 }
 Locale.Success = {
-
+    accepted = "Accepted the job! Meet your client at Bollingbroke.",
+    on_the_way = "A bondsman is on their way to help bail you out! Wait here.",
+    bailed_out = "You have been bailed out of prison! Stay safe.",
+    bailed_out_bondsman = "You successfully bailed out fullname.",
+    debt_paid = "You successfully paid all of your debt!",
+    debt_amount = "You paid $amount of your debt!",
+    set_bail_for = "You set fullname's bail for $amount",
 }
-Locale.Info = {
 
+Locale.Info = {
+    tablet_title = "Bailbonds Tablet",
+    available = "amount Available Bonds",
+    set_for = "Their bond is set for $amount",
+    months_left = "amount Months Left",
+    accept_job = "Accept Job",
+    accept_job_desc = "Accept this job for $amount",
+    go_back = "Back to main menu",
+    you_owe = "You owe us $amount more.",
+    payment_management = "Payment Management",
+    make_payment = "Make a payment",
+    make_payment_desc = "Pay off your bail, so we don't hunt you!",
+    dialog_title = "Choose how much you pay",
+    dialog_amount = "Amount",
+    owes = "Owes $amount",
+    laptop_title = "Bailbonds Laptop",
+    your_bail_is = "Your bail is set at $amount",
+    opening_tablet = "Opening Tablet...",
+    opening_laptop = "Opening Laptop...",
 }
